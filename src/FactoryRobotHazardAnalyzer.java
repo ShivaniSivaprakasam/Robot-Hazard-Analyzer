5a98c95dc82b9bb4d1dfaaf588d3532ee3bf4186
shivani <shivaniavanikkarasi@gmail.com>
2026-05-09 17:09:40 +0530

    [Shivani] : Added : Display static system message

---
+++ src/FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,15 @@
+/**
+ * UC1: Display Static Hazard Message
+ * -----------------------------------
+ * Goal: Show system name when application starts.
+ * Concepts: Class, main method, console output
+ */
+
+public class FactoryRobotHazardAnalyzer {
+
+    public static void main(String[] args) {
+
+        // UC1: System entry point message
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
